570d41108fa1575f53244fa15d50b72d674ed6ab
blackcoat77 <kimanovic@gmail.com>
2016-12-05 21:45:46 -0500

    Create 3 methods using Ruby arrays and its methods, in stattic_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,37 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
-  def help
-  end
+    helper_method :yeller
+    helper_method :random_subdomain
+    helper_method :string_shuffle
 
-  def about
-  end
+    def home
+    end
 
-  def contact
-  end
+    def help
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    # yeller method accepts array elements and returns upercase letters
+    def yeller(array = [])
+        if array.empty?
+            puts 'Array is empty'
+        else
+            array.map(&:upcase).join
+        end
+    end
+
+    # random_subdomain method returns a randomly generated strings of 8 charachters
+    def random_subdomain
+        ('a'..'z').to_a.shuffle[0..7].join
+    end
+
+    # string_shuffle method takes string and returns shuffled letters
+    def string_shuffle(s)
+      s.split('').shuffle.join
+    end
 end
