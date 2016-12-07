100ca0b5dd60d4078e52da5b5174352b0ea89bcb
blackcoat77 <kimanovic@gmail.com>
2016-12-06 23:47:14 -0500

    Create new method palindrome

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     helper_method :yeller
     helper_method :random_subdomain
     helper_method :string_shuffle
+    helper_method :palindrome?
 
     def home
     end
@@ -33,5 +34,14 @@ class StaticPagesController < ApplicationController
     # string_shuffle method takes string and returns shuffled letters
     def string_shuffle(s)
       s.split('').shuffle.join
+    end
+
+    # palindrome? method takes string, checks if it is palindrome and returns boolean true or false
+    def palindrome?(word)
+      if word == word.reverse
+        puts "The word #{word} is palindrome"
+      elsif
+        puts "The word #{word} is not palindrome"
+      end
     end
 end
