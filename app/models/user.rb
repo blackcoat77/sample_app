3d2b3cce2a7111336e02db6bb97229f6a07dac12
blackcoat77 <kimanovic@gmail.com>
2016-12-10 17:38:30 -0500

    Add before_save method in User mode, Create Test for email addresses to be saved in lowercase, Comment temporary data from user.yml, for testing purposes, Add indice in users table on :email column and make it unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+
+  # Use before_save to convert email letters to lowercase before saving it to DB
+  before_save { email.downcase! }
+
+  # the code from above can be written like code below. Expalnation why:
+  # The exclamation point ! is often used to indicate methods that mutate the
+  # object in question. In this case, email.downcase! modifies the email 
+  # attribute in place.
+  #
+  # before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
