66c24f7dee969780288c67f7ec228b7e89fab6ce
blackcoat77 <kimanovic@gmail.com>
2016-12-12 14:41:49 -0500

    Create methods log_in and current_user in sessions_helper to retain temporary cookie till user closes the browser, Include SessionHelper in ApplicationController so that helper can be available to all controllers, Fill out create method in sessions_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Include sessions helper module so that can be available through out app
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(password: params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
+      log_in user
+      redirect_to user
     else
       # create error message
-    flash.now[:danger] = 'Invalid email/password combination' 
+    flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
     end
   end
