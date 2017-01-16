5d7466aff9b43b2cc8ad2a946e99f155b5cf8f0f
blackcoat77 <kimanovic@gmail.com>
2017-01-16 11:58:21 -0500

    Fix multiple browser logout issue, Add forget method

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,13 @@ class User < ApplicationRecord
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
